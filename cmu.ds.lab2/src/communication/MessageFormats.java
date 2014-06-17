2e2d9c7030f7c10c3117210f383774b3e4144d28
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-06-17 16:47:16 -0400

    initial skeleton

---
+++ cmu.ds.lab2/src/communication/MessageFormats.java
@@ -0,0 +1,11 @@
+package communication;
+
+/*
+ * This class describes the common message formats that all 
+ * classes use for communication 
+ * 
+ */
+
+public class MessageFormats {
+	
+}
---
+++ cmu.ds.lab2/src/server/RegistryObject.java
@@ -0,0 +1,13 @@
+package server;
+
+public class RegistryObject {
+	String ip;
+	int portNumber;
+	
+	
+	public RegistryObject(String i, int p){
+		ip = i;
+		portNumber = p;
+	}
+	
+}
