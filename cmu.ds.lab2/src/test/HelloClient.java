98f29f6add45088d29155590acb869467ae0a19a
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-06-18 16:06:31 -0400

    Inbuilt JAVA RMI test files

---
+++ cmu.ds.lab2/src/test/HelloClient.java
@@ -0,0 +1,19 @@
+package test;
+import java.rmi.Naming;
+
+
+public class HelloClient {
+	  /**
+	   * Client program for the "Hello, world!" example.
+	   * @param argv The command line arguments which are ignored.
+	   */
+	  public static void main (String[] argv) {
+	    try {
+	      HelloInterface hello = 
+	        (HelloInterface) Naming.lookup ("//localhost/Hello123");
+	      System.out.println (hello.say());
+	    } catch (Exception e) {
+	      System.out.println ("HelloClient exception: " + e);
+	    }
+	  }
+}
---
+++ cmu.ds.lab2/src/test/HelloServer.java
@@ -0,0 +1,18 @@
+package test;
+import java.rmi.Naming;
+
+
+public class HelloServer {
+	  /**
+	   * Server program for the "Hello, world!" example.
+	   * @param argv The command line arguments which are ignored.
+	   */
+	  public static void main (String[] argv) {
+	    try {
+	      Naming.rebind ("Hello123", new Hello ("Hello, world!"));
+	      System.out.println ("Hello Server is ready.");
+	    } catch (Exception e) {
+	      System.out.println ("Hello Server failed: " + e);
+	    }
+	  }
+}
