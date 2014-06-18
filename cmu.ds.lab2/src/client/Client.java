550c174f84d18b939ec83105f4997cf7d5823b8a
Amey Ghadigaonkar <gamoholic010@gmail.com>
2014-06-18 15:39:59 -0400

    skeleton v1.1

--- cmu.ds.lab2/src/client/Client.java
+++ cmu.ds.lab2/src/client/Client.java
@@ -5,7 +5,9 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Client {
 	//this is to ensure that each remote object one and only one stub
 	//it will map bindname to client stub objects
-	static ConcurrentHashMap<String, ClientStub> remoteObjMap;
+	static ConcurrentHashMap<String, ClientStub> remoteObjMap 
+				= new ConcurrentHashMap<String,ClientStub>();
+	
 	
 	
 }
--- cmu.ds.lab2/src/communication/SendMessage.java
+++ cmu.ds.lab2/src/communication/SendMessage.java
@@ -1,7 +1,13 @@
 package communication;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class SendMessage {
 	static Object sendLock = new Object();
+	private static Socket sendingSocket;
 	
 	
 	/*
@@ -9,7 +15,20 @@ public class SendMessage {
 	 * @return -1 if call unsuccessful
 	 * @return 0 if call successful
 	 */
-	public static int SendMessage(String host, int port, Message m) {
-		Socket sendingSocket = new Socket(host,port);
+	public static int sendMessage(String hostName, int port, Message m) throws InterruptedException {
+		try {
+			sendingSocket = new Socket(InetAddress.getByName(hostName),port);
+			ObjectOutputStream os = new ObjectOutputStream(sendingSocket.getOutputStream());
+			os.writeObject(m);
+			Thread.sleep(200);
+			//close sending socket
+			sendingSocket.close();
+			return 0;
+			
+		} catch (IOException e) {
+			e.printStackTrace( );
+			return -1;
+		}
+		
 	}
 }
